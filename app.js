19a9e1ba7564beb523ee3bb6f103c197ebd197c5
OJ Ike <ojayyike@gmail.com>
2021-11-23 02:02:30 -0500

    add background color and css styling

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express();
 const port = 3008;
 const middleware = require('./middleware')
-
+const path = require('path')
 
 const server = app.listen(port, () => console.log("Server Listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views","views")
-
+//Any file stored inside the public folder is served as a public file
+app.use(express.static(path.join(__dirname, "public")))
 //Routes
 const loginRouter = require('./routes/loginRoutes');
+
 app.use("/login",loginRouter)
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
