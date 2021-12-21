e8d8a5acbb1b43bd0c805602a39bca627068943a
OJ Ike <ojayyike@gmail.com>
2021-12-20 21:08:53 -0500

    Change color of pin to indicate the post has been pinned by the user

--- public/js/common.js
+++ public/js/common.js
@@ -323,7 +323,11 @@ function createPostHtml(postData, largeFont = false) {
 
     var buttons = "";
     if (postData.postedBy._id == userLoggedIn._id) {
-        buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#confirmPinModal"><i class='fas fa-thumbtack'></i></button>
+        var pinnedClass = "";
+        if (postData.pinned === true) {
+            pinnedClass =  "active";
+        }
+        buttons = `<button class='pinButton ${pinnedClass}' data-id="${postData._id}" data-toggle="modal" data-target="#confirmPinModal"><i class='fas fa-thumbtack'></i></button>
                 <button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-trash-alt'></i></button>`
     }
 
